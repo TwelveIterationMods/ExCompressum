553e0b4fdbd3054a52eaa4b0a0d004bc89b2a450
Blay09 <blay09@hotmail.de>
2016-03-03 19:28:05 +0100

    Allow wood chippings for all rotations of wood.

--- src/main/java/net/blay09/mods/excompressum/item/ItemWoodChipping.java
+++ src/main/java/net/blay09/mods/excompressum/item/ItemWoodChipping.java
@@ -6,6 +6,7 @@ import exnihilo.registries.helpers.Color;
 import net.blay09.mods.excompressum.ExCompressum;
 import net.blay09.mods.excompressum.ModItems;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockRotatedPillar;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -26,9 +27,23 @@ public class ItemWoodChipping extends Item {
         if(config.getBoolean("Wood Chippings", "items", true, "If set to true, wood can be smashed into wood chippings, which can be composted into dirt.")) {
             List<ItemStack> woodLogs = OreDictionary.getOres("logWood", false);
             for(ItemStack itemStack : woodLogs) {
-                HammerRegistry.register(Block.getBlockFromItem(itemStack.getItem()), itemStack.getItemDamage(), ModItems.woodChipping, 0, 1f, 0f);
-                HammerRegistry.register(Block.getBlockFromItem(itemStack.getItem()), itemStack.getItemDamage(), ModItems.woodChipping, 0, 0.5f, 0f);
-                HammerRegistry.register(Block.getBlockFromItem(itemStack.getItem()), itemStack.getItemDamage(), ModItems.woodChipping, 0, 0.25f, 0f);
+                Block block = Block.getBlockFromItem(itemStack.getItem());
+                int metadata = itemStack.getItemDamage();
+                if(block instanceof BlockRotatedPillar) {
+                    HammerRegistry.register(block, metadata, ModItems.woodChipping, 0, 1f, 0f);
+                    HammerRegistry.register(block, metadata, ModItems.woodChipping, 0, 0.5f, 0f);
+                    HammerRegistry.register(block, metadata, ModItems.woodChipping, 0, 0.25f, 0f);
+                    HammerRegistry.register(block, metadata | 4, ModItems.woodChipping, 0, 1f, 0f);
+                    HammerRegistry.register(block, metadata | 4, ModItems.woodChipping, 0, 0.5f, 0f);
+                    HammerRegistry.register(block, metadata | 4, ModItems.woodChipping, 0, 0.25f, 0f);
+                    HammerRegistry.register(block, metadata | 8, ModItems.woodChipping, 0, 1f, 0f);
+                    HammerRegistry.register(block, metadata | 8, ModItems.woodChipping, 0, 0.5f, 0f);
+                    HammerRegistry.register(block, metadata | 8, ModItems.woodChipping, 0, 0.25f, 0f);
+                } else {
+                    HammerRegistry.register(block, metadata, ModItems.woodChipping, 0, 1f, 0f);
+                    HammerRegistry.register(block, metadata, ModItems.woodChipping, 0, 0.5f, 0f);
+                    HammerRegistry.register(block, metadata, ModItems.woodChipping, 0, 0.25f, 0f);
+                }
             }
             CompostRegistry.register(Item.getItemFromBlock(Blocks.brown_mushroom), 0, 0.125f, new Color("CFBFB6"));
         }
