b6b47f1545fa345961e14ad6da7e3664f2603435
Blay09 <blay09@hotmail.de>
2016-10-31 20:05:02 +0100

    Add TheOneProbe support

--- src/main/java/net/blay09/mods/excompressum/ExCompressum.java
+++ src/main/java/net/blay09/mods/excompressum/ExCompressum.java
@@ -49,140 +49,152 @@ import java.util.List;
 @SuppressWarnings("unused")
 public class ExCompressum {
 
-    public static final Logger logger = LogManager.getLogger();
-    public static final String MOD_ID = "excompressum";
+	public static final Logger logger = LogManager.getLogger();
+	public static final String MOD_ID = "excompressum";
 
-    @Mod.Instance
-    public static ExCompressum instance;
+	@Mod.Instance
+	public static ExCompressum instance;
 
-    @SidedProxy(serverSide = "net.blay09.mods.excompressum.CommonProxy", clientSide = "net.blay09.mods.excompressum.client.ClientProxy")
-    public static CommonProxy proxy;
+	@SidedProxy(serverSide = "net.blay09.mods.excompressum.CommonProxy", clientSide = "net.blay09.mods.excompressum.client.ClientProxy")
+	public static CommonProxy proxy;
 
-    public static File configDir;
-    private Configuration config;
+	public static File configDir;
+	private Configuration config;
 
-    public static final ExCompressumCreativeTab creativeTab = new ExCompressumCreativeTab();
+	public static final ExCompressumCreativeTab creativeTab = new ExCompressumCreativeTab();
 
-    public final List<IAddon> addons = Lists.newArrayList();
+	public final List<IAddon> addons = Lists.newArrayList();
 
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        configDir = new File(event.getModConfigurationDirectory(), "ExCompressum");
-        if(!configDir.exists() && !configDir.mkdirs()) {
-            throw new RuntimeException("Couldn't create Ex Compressum configuration directory");
-        }
-        config = new Configuration(new File(configDir, "ExCompressum.cfg"));
-        config.load();
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		configDir = new File(event.getModConfigurationDirectory(), "ExCompressum");
+		if (!configDir.exists() && !configDir.mkdirs()) {
+			throw new RuntimeException("Couldn't create Ex Compressum configuration directory");
+		}
+		config = new Configuration(new File(configDir, "ExCompressum.cfg"));
+		config.load();
 
-        ExCompressumConfig.load(config);
+		ExCompressumConfig.load(config);
 
-        ModItems.init();
-        ModBlocks.init();
+		ModItems.init();
+		ModBlocks.init();
 
-        EntityRegistry.registerModEntity(EntityAngryChicken.class, "AngryChicken", 0, this, 64, 3, true);
+		EntityRegistry.registerModEntity(EntityAngryChicken.class, "AngryChicken", 0, this, 64, 3, true);
 
-        NetworkRegistry.INSTANCE.registerGuiHandler(this, new GuiHandler());
-        MinecraftForge.EVENT_BUS.register(this);
-        MinecraftForge.EVENT_BUS.register(new HammerHandler());
-        MinecraftForge.EVENT_BUS.register(new CompressedHammerHandler());
-        MinecraftForge.EVENT_BUS.register(new CompressedCrookHandler());
-        MinecraftForge.EVENT_BUS.register(new CompressedEnemyHandler());
-        MinecraftForge.EVENT_BUS.register(new ChickenStickHandler());
+		NetworkRegistry.INSTANCE.registerGuiHandler(this, new GuiHandler());
+		MinecraftForge.EVENT_BUS.register(this);
+		MinecraftForge.EVENT_BUS.register(new HammerHandler());
+		MinecraftForge.EVENT_BUS.register(new CompressedHammerHandler());
+		MinecraftForge.EVENT_BUS.register(new CompressedCrookHandler());
+		MinecraftForge.EVENT_BUS.register(new CompressedEnemyHandler());
+		MinecraftForge.EVENT_BUS.register(new ChickenStickHandler());
 
-        // NOTE Botania is a special snowflake that requires addon initialization during preInit
-        if(Loader.isModLoaded(Compat.BOTANIA)) {
-            try {
-                Class<?> clazz = Class.forName("net.blay09.mods.excompressum.compat.botania.BotaniaAddon");
-                addons.add((IAddon) clazz.getConstructor(Configuration.class).newInstance(config));
-            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
-                ExCompressum.logger.error("Failed to load Botania addon: {}", e);
-            }
-        }
+		// NOTE Botania is a special snowflake that requires addon initialization during preInit
+		if (Loader.isModLoaded(Compat.BOTANIA)) {
+			try {
+				Class<?> clazz = Class.forName("net.blay09.mods.excompressum.compat.botania.BotaniaAddon");
+				addons.add((IAddon) clazz.getConstructor(Configuration.class).newInstance(config));
+			} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+				ExCompressum.logger.error("Failed to load Botania addon: {}", e);
+			}
+		}
 
-        proxy.preInit(event);
+		proxy.preInit(event);
 
-        // NOTE Botania decided to be a special snowflake once more
-        for(IAddon addon : addons) {
-            proxy.preInitAddon(addon);
-        }
-    }
+		// NOTE Botania decided to be a special snowflake once more
+		for (IAddon addon : addons) {
+			proxy.preInitAddon(addon);
+		}
+	}
 
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-//        FMLInterModComms.sendFunctionMessage(Compat.THEONEPROBE, "getTheOneProbe", "net.blay09.mods.excompressum.compat.top.ThisAPI$IsTerrible"); // TODO get back to this later
-        FMLInterModComms.sendMessage(Compat.WAILA, "register", "net.blay09.mods.excompressum.compat.waila.WailaProvider.register");
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event) {
+		FMLInterModComms.sendFunctionMessage(Compat.THEONEPROBE, "getTheOneProbe", "net.blay09.mods.excompressum.compat.top.TheOneProbeAddon");
+		FMLInterModComms.sendMessage(Compat.WAILA, "register", "net.blay09.mods.excompressum.compat.waila.WailaProvider.register");
 
-        ModRecipes.init();
+		registerAddon(Compat.EXNIHILO_OMNIA, "net.blay09.mods.excompressum.compat.exnihiloomnia.ExNihiloOmniaAddon");
+		registerAddon(Compat.EXNIHILO_ADSCENSIO, "net.blay09.mods.excompressum.compat.exnihiloadscensio.ExNihiloAdscensioAddon");
 
-        proxy.init(event);
-    }
+		ModRecipes.init();
 
-    @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
-        ExCompressumConfig.postLoad(config);
+		proxy.init(event);
+	}
 
-        registerAddon(event, Compat.EXNIHILO_OMNIA, "net.blay09.mods.excompressum.compat.exnihiloomnia.ExNihiloOmniaAddon");
-        registerAddon(event, Compat.EXNIHILO_ADSCENSIO, "net.blay09.mods.excompressum.compat.exnihiloadscensio.ExNihiloAdscensioAddon");
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		ExCompressumConfig.postLoad(config);
 
-        if(ExRegistro.instance == null) {
-            ExCompressum.logger.warn("No Ex Nihilo mod installed - many things will be disabled. Why would you run Ex Compressum without Ex Nihilo? Pfft.");
-            ExRegistro.instance = new NihilisticNihiloProvider();
-        }
+		if (ExRegistro.instance == null) {
+			ExCompressum.logger.warn("No Ex Nihilo mod installed - many things will be disabled. Why would you run Ex Compressum without Ex Nihilo? Pfft.");
+			ExRegistro.instance = new NihilisticNihiloProvider();
+		}
 
-        ChickenStickRegistry.INSTANCE.load(configDir);
-        WoodenCrucibleRegistry.INSTANCE.load(configDir);
-        CompressedHammerRegistry.INSTANCE.load(configDir);
-        HeavySieveRegistry.INSTANCE.load(configDir);
+		ChickenStickRegistry.INSTANCE.load(configDir);
+		WoodenCrucibleRegistry.INSTANCE.load(configDir);
+		CompressedHammerRegistry.INSTANCE.load(configDir);
+		HeavySieveRegistry.INSTANCE.load(configDir);
 
-        CompressedRecipeRegistry.reload();
+		CompressedRecipeRegistry.reload();
 
-        SieveMeshRegistry.registerDefaults();
-        AutoSieveSkinRegistry.load();
+		SieveMeshRegistry.registerDefaults();
+		AutoSieveSkinRegistry.load();
 
-        registerAddon(event, Compat.MINETWEAKER, "net.blay09.mods.excompressum.compat.minetweaker.MineTweakerAddon");
-        registerAddon(event, Compat.TCONSTRUCT, "net.blay09.mods.excompressum.compat.tconstruct.TConstructAddon");
+		registerAddon(Compat.MINETWEAKER, "net.blay09.mods.excompressum.compat.minetweaker.MineTweakerAddon");
+		registerAddon(Compat.TCONSTRUCT, "net.blay09.mods.excompressum.compat.tconstruct.TConstructAddon");
 
-        for(IAddon addon : addons) {
-            addon.loadConfig(config);
-            addon.postInit();
-        }
+		for (IAddon addon : addons) {
+			addon.loadConfig(config);
+			addon.postInit();
+		}
 
-        if(config.hasChanged()) {
-            config.save();
-        }
+		if (config.hasChanged()) {
+			config.save();
+		}
 
-        proxy.postInit(event);
-    }
+		proxy.postInit(event);
+	}
 
-    private void registerAddon(FMLPostInitializationEvent event, String modid, String className) {
-        Optional<?> optional = event.buildSoftDependProxy(modid, className);
-        if(optional.isPresent()) {
-            addons.add((IAddon) optional.get());
-        }
-    }
+	private Optional<?> buildSoftDependProxy(String modId, String className, Object... arguments) {
+		if (Loader.isModLoaded(modId)) {
+			try {
+				Class<?> clz = Class.forName(className, true, Loader.instance().getModClassLoader());
+				return Optional.fromNullable(clz.newInstance());
+			} catch (Exception e) {
+				return Optional.absent();
+			}
+		}
+		return Optional.absent();
+	}
 
-    @Mod.EventHandler
-    public void serverStarting(FMLServerStartingEvent event) {
-        event.registerServerCommand(new CommandExCompressum());
-    }
+	private void registerAddon(String modid, String className) {
+		Optional<?> optional = buildSoftDependProxy(modid, className);
+		if (optional.isPresent()) {
+			addons.add((IAddon) optional.get());
+		}
+	}
 
-    @Mod.EventHandler
-    public void serverStarted(FMLServerStartedEvent event) {
-        for(IAddon addon : addons) {
-            addon.serverStarted(event);
-        }
-    }
+	@Mod.EventHandler
+	public void serverStarting(FMLServerStartingEvent event) {
+		event.registerServerCommand(new CommandExCompressum());
+	}
 
-    @SubscribeEvent
-    public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
-        if(AbstractRegistry.registryErrors.size() > 0) {
-            event.player.addChatComponentMessage(new TextComponentString(TextFormatting.RED + "There were errors loading the Ex Compressum registries:"));
-            TextFormatting lastFormatting = TextFormatting.WHITE;
-            for(String error : AbstractRegistry.registryErrors) {
-                event.player.addChatMessage(new TextComponentString(lastFormatting + "* " + error));
-                lastFormatting = lastFormatting == TextFormatting.GRAY ? TextFormatting.WHITE : TextFormatting.GRAY;
-            }
-        }
-    }
+	@Mod.EventHandler
+	public void serverStarted(FMLServerStartedEvent event) {
+		for (IAddon addon : addons) {
+			addon.serverStarted(event);
+		}
+	}
+
+	@SubscribeEvent
+	public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
+		if (AbstractRegistry.registryErrors.size() > 0) {
+			event.player.addChatComponentMessage(new TextComponentString(TextFormatting.RED + "There were errors loading the Ex Compressum registries:"));
+			TextFormatting lastFormatting = TextFormatting.WHITE;
+			for (String error : AbstractRegistry.registryErrors) {
+				event.player.addChatMessage(new TextComponentString(lastFormatting + "* " + error));
+				lastFormatting = lastFormatting == TextFormatting.GRAY ? TextFormatting.WHITE : TextFormatting.GRAY;
+			}
+		}
+	}
 
 }
--- src/main/java/net/blay09/mods/excompressum/compat/top/ThisAPI.java
+++
@@ -1,6 +0,0 @@
-package net.blay09.mods.excompressum.compat.top;
-
-import com.google.common.base.Function;
-
-public class ThisAPI {
-}
