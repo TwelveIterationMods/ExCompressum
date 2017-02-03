104688da0de8228efe68a18ad206ad447ef913e6
Blay09 <blay09@hotmail.de>
2017-02-03 11:32:13 +0100

    Fix compressed hammer destroying unhammerable blocks instead #162

--- src/main/java/net/blay09/mods/excompressum/handler/CompressedHammerHandler.java
+++ src/main/java/net/blay09/mods/excompressum/handler/CompressedHammerHandler.java
@@ -1,11 +1,14 @@
 package net.blay09.mods.excompressum.handler;
 
+import com.google.common.collect.Lists;
 import net.blay09.mods.excompressum.item.ICompressedHammer;
 import net.blay09.mods.excompressum.registry.ExRegistro;
 import net.blay09.mods.excompressum.registry.compressedhammer.CompressedHammerRegistry;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.List;
 
 public class CompressedHammerHandler {
 
@@ -15,10 +18,13 @@ public class CompressedHammerHandler {
 			ItemStack heldItem = event.getHarvester().getHeldItemMainhand();
 			if(heldItem != null && heldItem.getItem() instanceof ICompressedHammer && ((ICompressedHammer) heldItem.getItem()).canHammer(heldItem, event.getWorld(), event.getState(), event.getHarvester())) {
 				int fortune = event.getFortuneLevel();
-				event.setDropChance(1f);
-				event.getDrops().clear();
-				event.getDrops().addAll(CompressedHammerRegistry.rollHammerRewards(event.getState(), fortune, event.getWorld().rand));
-				event.getDrops().addAll(ExRegistro.rollHammerRewards(event.getState(), ((ICompressedHammer) heldItem.getItem()).getHammerLevel(heldItem, event.getWorld(), event.getState(), event.getHarvester()), fortune, event.getWorld().rand));
+				List<ItemStack> rewards = Lists.newArrayList(CompressedHammerRegistry.rollHammerRewards(event.getState(), fortune, event.getWorld().rand));
+				rewards.addAll(ExRegistro.rollHammerRewards(event.getState(), ((ICompressedHammer) heldItem.getItem()).getHammerLevel(heldItem, event.getWorld(), event.getState(), event.getHarvester()), fortune, event.getWorld().rand));
+				if(!rewards.isEmpty()) {
+					event.setDropChance(1f);
+					event.getDrops().clear();
+					event.getDrops().addAll(rewards);
+				}
 			}
 		}
 	}
