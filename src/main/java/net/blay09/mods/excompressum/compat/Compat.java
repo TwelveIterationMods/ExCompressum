8eb53b7e9a23f511b0fee5b5382d498ba44d02d7
Blay09 <blay09@hotmail.de>
2017-02-03 11:39:36 +0100

    Extra Utilities modid is now lowercase #175 #173

--- src/main/java/net/blay09/mods/excompressum/compat/Compat.java
+++ src/main/java/net/blay09/mods/excompressum/compat/Compat.java
@@ -7,7 +7,7 @@ public class Compat {
 	public static final String EXNIHILO_ADSCENSIO = "exnihiloadscensio";
 	public static final String TCONSTRUCT = "tconstruct";
 	public static final String MINETWEAKER = "MineTweaker3";
-	public static final String EXTRAUTILS2 = "ExtraUtils2";
+	public static final String EXTRAUTILS2 = "extrautils2";
 	public static final String WAILA = "Waila";
 	public static final String JEI = "JEI";
 	public static final String THEONEPROBE = "theoneprobe";
