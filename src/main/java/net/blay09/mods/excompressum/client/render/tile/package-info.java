0d7469d8628782d1876d97f8931235d206cde68d
Blay09 <blay09@hotmail.de>
2017-06-12 11:13:19 +0200

    Add missing package.infos

---
+++ src/main/java/net/blay09/mods/excompressum/client/render/tile/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package net.blay09.mods.excompressum.client.render.tile;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ src/main/java/net/blay09/mods/excompressum/compat/botania/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package net.blay09.mods.excompressum.compat.botania;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
