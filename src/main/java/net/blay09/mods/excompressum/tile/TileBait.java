424b7926bcaad85a9223a01bb9d468e92530a7b3
blay09 <blay09@hotmail.de>
2018-06-08 13:21:34 +0200

    Avoid crash due to nonsensical null values that should never be able to happen but for some reason it's happening
    #240

--- src/main/java/net/blay09/mods/excompressum/tile/TileBait.java
+++ src/main/java/net/blay09/mods/excompressum/tile/TileBait.java
@@ -25,164 +25,172 @@ import java.util.Collection;
 
 public class TileBait extends TileEntity implements ITickable {
 
-	private static final int ENVIRONMENTAL_CHECK_INTERVAL = 20 * 10;
-	private static final int MAX_BAITS_IN_AREA = 2;
-	private static final int MIN_ENV_IN_AREA = 10;
-	private static final int MAX_ANIMALS_IN_AREA = 2;
-	private static final int SPAWN_CHECK_INTERVAL = 20;
-	private static final int MIN_DISTANCE_NO_PLAYERS = 6;
+    private static final int ENVIRONMENTAL_CHECK_INTERVAL = 20 * 10;
+    private static final int MAX_BAITS_IN_AREA = 2;
+    private static final int MIN_ENV_IN_AREA = 10;
+    private static final int MAX_ANIMALS_IN_AREA = 2;
+    private static final int SPAWN_CHECK_INTERVAL = 20;
+    private static final int MIN_DISTANCE_NO_PLAYERS = 6;
 
-	public enum EnvironmentalCondition {
-		CanSpawn("info.excompressum:baitCanSpawn"),
-		NearbyBait("info.excompressum:baitNearbyBait"),
-		WrongEnv("info.excompressum:baitWrongEnv"),
-		NearbyAnimal("info.excompressum:baitNearbyAnimal"),
-		NoWater("info.excompressum:baitNoWater");
+    public enum EnvironmentalCondition {
+        CanSpawn("info.excompressum:baitCanSpawn"),
+        NearbyBait("info.excompressum:baitNearbyBait"),
+        WrongEnv("info.excompressum:baitWrongEnv"),
+        NearbyAnimal("info.excompressum:baitNearbyAnimal"),
+        NoWater("info.excompressum:baitNoWater");
 
-		public final String langKey;
+        public final String langKey;
 
-		EnvironmentalCondition(String langKey) {
-			this.langKey = langKey;
-		}
-	}
+        EnvironmentalCondition(String langKey) {
+            this.langKey = langKey;
+        }
+    }
 
-	public static class BaitBlockCondition {
-		private final IBlockState state;
-		private final boolean isWildcard;
+    public static class BaitBlockCondition {
+        private final IBlockState state;
+        private final boolean isWildcard;
 
-		public BaitBlockCondition(IBlockState state, boolean isWildcard) {
-			this.state = state;
-			this.isWildcard = isWildcard;
-		}
+        public BaitBlockCondition(IBlockState state, boolean isWildcard) {
+            this.state = state;
+            this.isWildcard = isWildcard;
+        }
 
-		public IBlockState getState() {
-			return state;
-		}
+        public IBlockState getState() {
+            return state;
+        }
 
-		public boolean isWildcard() {
-			return isWildcard;
-		}
-	}
+        public boolean isWildcard() {
+            return isWildcard;
+        }
+    }
 
-	private ItemStack renderItemMain = ItemStack.EMPTY;
-	private ItemStack renderItemSub = ItemStack.EMPTY;
-	private EnvironmentalCondition environmentStatus;
-	private int ticksSinceEnvironmentalCheck;
-	private int ticksSinceSpawnCheck;
+    private ItemStack renderItemMain = ItemStack.EMPTY;
+    private ItemStack renderItemSub = ItemStack.EMPTY;
+    private EnvironmentalCondition environmentStatus;
+    private int ticksSinceEnvironmentalCheck;
+    private int ticksSinceSpawnCheck;
 
-	@Override
-	public void update() {
-		if (renderItemMain.isEmpty()) {
-			renderItemMain = getBaitDisplayItem(getBlockMetadata(), 0);
-		}
-		if (renderItemSub.isEmpty()) {
-			renderItemSub = getBaitDisplayItem(getBlockMetadata(), 1);
-		}
+    @Override
+    public void update() {
+        if (renderItemMain.isEmpty()) {
+            renderItemMain = getBaitDisplayItem(getBlockMetadata(), 0);
+        }
+        if (renderItemSub.isEmpty()) {
+            renderItemSub = getBaitDisplayItem(getBlockMetadata(), 1);
+        }
 
-		ticksSinceEnvironmentalCheck++;
+        ticksSinceEnvironmentalCheck++;
 
-		ticksSinceSpawnCheck++;
-		if (ticksSinceSpawnCheck >= SPAWN_CHECK_INTERVAL) {
-			int metadata = getBlockMetadata();
-			if (!world.isRemote && world.rand.nextFloat() <= getBaitChance(metadata)) {
-				if (checkSpawnConditions(true) == EnvironmentalCondition.CanSpawn) {
-					final float range = MIN_DISTANCE_NO_PLAYERS;
-					if (world.getEntitiesWithinAABB(EntityPlayer.class, new AxisAlignedBB(pos.getX() - range, pos.getY() - range, pos.getZ() - range, pos.getX() + range, pos.getY() + range, pos.getZ() + range)).isEmpty()) {
-						EntityLiving entityLiving = getBaitEntityLiving(world, metadata);
-						if (entityLiving != null) {
-							if (entityLiving instanceof EntityAgeable && world.rand.nextFloat() <= ModConfig.baits.childChance) {
-								((EntityAgeable) entityLiving).setGrowingAge(-24000);
-							}
-							entityLiving.setPosition(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5);
-							world.spawnEntity(entityLiving);
-							((WorldServer) world).spawnParticle(EnumParticleTypes.EXPLOSION_LARGE, false, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 1, 0, 0, 0, 0.0);
-							world.playSound(null, pos, SoundEvents.ENTITY_CHICKEN_EGG, SoundCategory.BLOCKS, 1f, 1f);
-						}
-						world.setBlockToAir(pos);
-					}
-				}
-			}
-			ticksSinceSpawnCheck = 0;
-		}
-	}
+        ticksSinceSpawnCheck++;
+        if (ticksSinceSpawnCheck >= SPAWN_CHECK_INTERVAL) {
+            int metadata = getBlockMetadata();
+            if (!world.isRemote && world.rand.nextFloat() <= getBaitChance(metadata)) {
+                if (checkSpawnConditions(true) == EnvironmentalCondition.CanSpawn) {
+                    final float range = MIN_DISTANCE_NO_PLAYERS;
+                    if (world.getEntitiesWithinAABB(EntityPlayer.class, new AxisAlignedBB(pos.getX() - range, pos.getY() - range, pos.getZ() - range, pos.getX() + range, pos.getY() + range, pos.getZ() + range)).isEmpty()) {
+                        EntityLiving entityLiving = getBaitEntityLiving(world, metadata);
+                        if (entityLiving != null) {
+                            if (entityLiving instanceof EntityAgeable && world.rand.nextFloat() <= ModConfig.baits.childChance) {
+                                ((EntityAgeable) entityLiving).setGrowingAge(-24000);
+                            }
+                            entityLiving.setPosition(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5);
+                            world.spawnEntity(entityLiving);
+                            ((WorldServer) world).spawnParticle(EnumParticleTypes.EXPLOSION_LARGE, false, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 1, 0, 0, 0, 0.0);
+                            world.playSound(null, pos, SoundEvents.ENTITY_CHICKEN_EGG, SoundCategory.BLOCKS, 1f, 1f);
+                        }
+                        world.setBlockToAir(pos);
+                    }
+                }
+            }
+            ticksSinceSpawnCheck = 0;
+        }
+    }
 
-	private static ItemStack getBaitDisplayItem(int metadata, int i) {
-		BlockBait.Type type = BlockBait.Type.fromId(metadata);
-		if(type != null) {
-			return i == 0 ? type.getDisplayItemFirst() : type.getDisplayItemSecond();
-		}
-		return ItemStack.EMPTY;
-	}
+    private static ItemStack getBaitDisplayItem(int metadata, int i) {
+        BlockBait.Type type = BlockBait.Type.fromId(metadata);
+        if (type != null) {
+            return i == 0 ? type.getDisplayItemFirst() : type.getDisplayItemSecond();
+        }
+        return ItemStack.EMPTY;
+    }
 
-	@Nullable
-	private static EntityLiving getBaitEntityLiving(World world, int metadata) {
-		BlockBait.Type type = BlockBait.Type.fromId(metadata);
-		return type != null ? type.createEntity(world) : null;
-	}
+    @Nullable
+    private static EntityLiving getBaitEntityLiving(World world, int metadata) {
+        BlockBait.Type type = BlockBait.Type.fromId(metadata);
+        return type != null ? type.createEntity(world) : null;
+    }
 
-	private float getBaitChance(int metadata) {
-		BlockBait.Type type = BlockBait.Type.fromId(metadata);
-		return type != null ? type.getChance() : 0f;
-	}
+    private float getBaitChance(int metadata) {
+        BlockBait.Type type = BlockBait.Type.fromId(metadata);
+        return type != null ? type.getChance() : 0f;
+    }
 
-	public ItemStack getRenderItem(int i) {
-		return i == 0 ? renderItemMain : renderItemSub;
-	}
+    public ItemStack getRenderItem(int i) {
+        return i == 0 ? renderItemMain : renderItemSub;
+    }
 
-	public EnvironmentalCondition checkSpawnConditions(boolean checkNow) {
-		if (checkNow || ticksSinceEnvironmentalCheck > ENVIRONMENTAL_CHECK_INTERVAL) {
-			int metadata = getBlockMetadata();
-			BlockBait.Type baitType = BlockBait.Type.fromId(metadata);
-			Collection<BaitBlockCondition> envBlocks = baitType != null ? baitType.getEnvironmentConditions() : null;
-			if(envBlocks == null) {
-				throw new RuntimeException("The bait you placed (metadata " + metadata + ") was not properly defined - this should not happen - please report to the mod developer!");
-			}
-			final int range = 5;
-			final int rangeVertical = 3;
-			int countBait = 0;
-			int countEnvBlocks = 0;
-			boolean foundWater = false;
-			for (int x = pos.getX() - range; x < pos.getX() + range; x++) {
-				for (int y = pos.getY() - rangeVertical; y < pos.getY() + rangeVertical; y++) {
-					for (int z = pos.getZ() - range; z < pos.getZ() + range; z++) {
-						BlockPos testPos = new BlockPos(x, y, z);
-						IBlockState state = world.getBlockState(testPos);
-						if (state.getBlock() == ModBlocks.bait) {
-							countBait++;
-						} else if (state.getBlock() == Blocks.WATER || state.getBlock() == Blocks.FLOWING_WATER) {
-							foundWater = true;
-						}
+    public EnvironmentalCondition checkSpawnConditions(boolean checkNow) {
+        if (checkNow || ticksSinceEnvironmentalCheck > ENVIRONMENTAL_CHECK_INTERVAL) {
+            int metadata = getBlockMetadata();
+            BlockBait.Type baitType = BlockBait.Type.fromId(metadata);
+            Collection<BaitBlockCondition> envBlocks = baitType != null ? baitType.getEnvironmentConditions() : null;
+            if (envBlocks == null) {
+                throw new RuntimeException("The bait you placed (metadata " + metadata + ") was not properly defined - this should not happen - please report to the mod developer!");
+            }
+            final int range = 5;
+            final int rangeVertical = 3;
+            int countBait = 0;
+            int countEnvBlocks = 0;
+            boolean foundWater = false;
+            for (int x = pos.getX() - range; x < pos.getX() + range; x++) {
+                for (int y = pos.getY() - rangeVertical; y < pos.getY() + rangeVertical; y++) {
+                    for (int z = pos.getZ() - range; z < pos.getZ() + range; z++) {
+                        BlockPos testPos = new BlockPos(x, y, z);
+                        IBlockState state = world.getBlockState(testPos);
+                        if (state.getBlock() == ModBlocks.bait) {
+                            countBait++;
+                        } else if (state.getBlock() == Blocks.WATER || state.getBlock() == Blocks.FLOWING_WATER) {
+                            foundWater = true;
+                        }
 
-						for (BaitBlockCondition envBlock : envBlocks) {
-							if (state.getBlock() == envBlock.getState().getBlock()) {
-								if (!envBlock.isWildcard()) {
-									int meta = state.getBlock().getMetaFromState(state);
-									int envMeta = envBlock.getState().getBlock().getMetaFromState(envBlock.getState());
-									if (meta != envMeta) {
-										continue;
-									}
-								}
-								countEnvBlocks++;
-							}
-						}
-					}
-				}
-			}
-			if (!foundWater) {
-				environmentStatus = EnvironmentalCondition.NoWater;
-			} else if (countBait > MAX_BAITS_IN_AREA) {
-				environmentStatus = EnvironmentalCondition.NearbyBait;
-			} else if (countEnvBlocks < MIN_ENV_IN_AREA) {
-				environmentStatus = EnvironmentalCondition.WrongEnv;
-			} else if (world.getEntitiesWithinAABB(EntityAnimal.class, new AxisAlignedBB(pos.getX() - range * 2, pos.getY() - rangeVertical, pos.getZ() - range * 2, pos.getX() + range * 2, pos.getY() + rangeVertical, pos.getZ() + range * 2)).size() > MAX_ANIMALS_IN_AREA) {
-				environmentStatus = EnvironmentalCondition.NearbyAnimal;
-			} else {
-				environmentStatus = EnvironmentalCondition.CanSpawn;
-			}
-			ticksSinceEnvironmentalCheck = 0;
-		}
+                        for (BaitBlockCondition envBlock : envBlocks) {
+                            // There's something really weird happening where something here is null even though it's impossible.
+                            // If that happens, just satisfy the condition no matter what to prevent a crash and prevent baits from not working.
+                            // This will hopefully fix itself when baits become data-pack driven in 1.13.
+                            if (envBlock == null || envBlock.getState() == null) {
+                                countEnvBlocks++;
+								continue;
+                            }
 
-		return environmentStatus;
-	}
+                            if (state.getBlock() == envBlock.getState().getBlock()) {
+                                if (!envBlock.isWildcard()) {
+                                    int meta = state.getBlock().getMetaFromState(state);
+                                    int envMeta = envBlock.getState().getBlock().getMetaFromState(envBlock.getState());
+                                    if (meta != envMeta) {
+                                        continue;
+                                    }
+                                }
+                                countEnvBlocks++;
+                            }
+                        }
+                    }
+                }
+            }
+            if (!foundWater) {
+                environmentStatus = EnvironmentalCondition.NoWater;
+            } else if (countBait > MAX_BAITS_IN_AREA) {
+                environmentStatus = EnvironmentalCondition.NearbyBait;
+            } else if (countEnvBlocks < MIN_ENV_IN_AREA) {
+                environmentStatus = EnvironmentalCondition.WrongEnv;
+            } else if (world.getEntitiesWithinAABB(EntityAnimal.class, new AxisAlignedBB(pos.getX() - range * 2, pos.getY() - rangeVertical, pos.getZ() - range * 2, pos.getX() + range * 2, pos.getY() + rangeVertical, pos.getZ() + range * 2)).size() > MAX_ANIMALS_IN_AREA) {
+                environmentStatus = EnvironmentalCondition.NearbyAnimal;
+            } else {
+                environmentStatus = EnvironmentalCondition.CanSpawn;
+            }
+            ticksSinceEnvironmentalCheck = 0;
+        }
+
+        return environmentStatus;
+    }
 
 }
