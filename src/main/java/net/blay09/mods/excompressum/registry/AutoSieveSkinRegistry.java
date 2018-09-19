5d1bbd9f71d673cdab5864f4f4b3e3432769f300
blay09 <blay09@hotmail.de>
2018-09-19 19:40:03 +0200

    Thread safety

--- src/main/java/net/blay09/mods/excompressum/registry/AutoSieveSkinRegistry.java
+++ src/main/java/net/blay09/mods/excompressum/registry/AutoSieveSkinRegistry.java
@@ -16,44 +16,51 @@ import java.util.Random;
 
 public class AutoSieveSkinRegistry {
 
-	private static final Random random = new Random();
-	private static final List<String> availableSkins = Lists.newArrayList();
+    private static final Random random = new Random();
+    private static final List<String> availableSkins = Lists.newArrayList();
 
-	public static void load() {
-		if (!ModConfig.client.skipAutoSieveSkins) {
-			availableSkins.clear();
-			Thread loadAutoSieveSkins = new Thread(() -> {
-				try {
-					URL remoteURL = new URL("http://blay09.net/mods/control-panel/api/skins_v2.php");
-					InputStream in = remoteURL.openStream();
-					Gson gson = new Gson();
-					JsonReader reader = new JsonReader(new InputStreamReader(in));
-					JsonObject root = gson.fromJson(reader, JsonObject.class);
-					if (root.has("error")) {
-						ExCompressum.logger.error("Could not load remote skins for auto sieve: {}", root.get("error").getAsString());
-						return;
-					}
-					if (root.has("skins")) {
-						JsonArray skins = root.getAsJsonArray("skins");
-						for (int i = 0; i < skins.size(); i++) {
-							JsonObject skin = skins.get(i).getAsJsonObject();
-							availableSkins.add(skin.get("name").getAsString());
-						}
-					}
-					reader.close();
-				} catch (Throwable e) { // Screw it, let's just be overprotective.
-					ExCompressum.logger.error("Could not load remote skins for auto sieve: ", e);
-				}
-			});
-			loadAutoSieveSkins.start();
-		}
-	}
+    public static void load() {
+        if (!ModConfig.client.skipAutoSieveSkins) {
+            availableSkins.clear();
+            Thread loadAutoSieveSkins = new Thread(() -> {
+                try {
+                    URL remoteURL = new URL("http://blay09.net/mods/control-panel/api/skins_v2.php");
+                    InputStream in = remoteURL.openStream();
+                    Gson gson = new Gson();
+                    JsonReader reader = new JsonReader(new InputStreamReader(in));
+                    JsonObject root = gson.fromJson(reader, JsonObject.class);
+                    if (root.has("error")) {
+                        ExCompressum.logger.error("Could not load remote skins for auto sieve: {}", root.get("error").getAsString());
+                        return;
+                    }
 
-	public static String getRandomSkin() {
-		if (availableSkins.isEmpty()) {
-			availableSkins.add("Steve");
-		}
-		return availableSkins.get(random.nextInt(availableSkins.size()));
-	}
+                    if (root.has("skins")) {
+                        JsonArray skins = root.getAsJsonArray("skins");
+                        for (int i = 0; i < skins.size(); i++) {
+                            JsonObject skin = skins.get(i).getAsJsonObject();
+                            synchronized (availableSkins) {
+                                availableSkins.add(skin.get("name").getAsString());
+                            }
+                        }
+                    }
+
+                    reader.close();
+                } catch (Throwable e) { // Screw it, let's just be overprotective.
+                    ExCompressum.logger.error("Could not load remote skins for auto sieve: ", e);
+                }
+            });
+            loadAutoSieveSkins.start();
+        }
+    }
+
+    public static String getRandomSkin() {
+        synchronized (availableSkins) {
+            if (availableSkins.isEmpty()) {
+                availableSkins.add("Steve");
+            }
+
+            return availableSkins.get(random.nextInt(availableSkins.size()));
+        }
+    }
 
 }
