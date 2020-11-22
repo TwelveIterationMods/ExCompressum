897424b31b8cfd2a67a7e4b923956f090241bc28
blay09 <blay09@hotmail.de>
2020-11-22 16:48:51 +0100

    New bait graphics, new baits added

--- src/main/java/net/blay09/mods/excompressum/block/BaitType.java
+++ src/main/java/net/blay09/mods/excompressum/block/BaitType.java
@@ -25,35 +25,39 @@ import java.util.Locale;
 import java.util.function.Supplier;
 
 public enum BaitType implements IStringSerializable {
-    WOLF(new ItemStack(Items.BEEF), new ItemStack(Items.BONE), EntityType.WOLF, ExCompressumConfig.COMMON.wolfBaitChance::get),
-    OCELOT(new ItemStack(Items.GUNPOWDER), new ItemStack(Items.COD), EntityType.OCELOT, ExCompressumConfig.COMMON.ocelotBaitChance::get),
-    COW(new ItemStack(Items.WHEAT), new ItemStack(Items.WHEAT), EntityType.COW, ExCompressumConfig.COMMON.cowBaitChance::get),
-    PIG(new ItemStack(Items.CARROT), new ItemStack(Items.CARROT), EntityType.PIG, ExCompressumConfig.COMMON.pigBaitChance::get),
-    CHICKEN(new ItemStack(Items.WHEAT_SEEDS), new ItemStack(Items.WHEAT_SEEDS), EntityType.CHICKEN, ExCompressumConfig.COMMON.chickenBaitChance::get),
-    SHEEP(new ItemStack(Items.WHEAT_SEEDS), new ItemStack(Items.WHEAT), EntityType.SHEEP, ExCompressumConfig.COMMON.sheepBaitChance::get),
-    SQUID(new ItemStack(Items.COD), new ItemStack(Items.COD), EntityType.SQUID, ExCompressumConfig.COMMON.squidBaitChance::get),
-    RABBIT(new ItemStack(Items.CARROT), new ItemStack(Items.MELON_SEEDS), EntityType.RABBIT, ExCompressumConfig.COMMON.rabbitBaitChance::get),
-    HORSE(new ItemStack(Items.GOLDEN_APPLE), new ItemStack(Items.GOLDEN_APPLE), EntityType.HORSE, ExCompressumConfig.COMMON.horseBaitChance::get),
-    DONKEY(new ItemStack(Items.GOLDEN_CARROT), new ItemStack(Items.GOLDEN_CARROT), EntityType.DONKEY, ExCompressumConfig.COMMON.donkeyBaitChance::get),
-    POLAR_BEAR(new ItemStack(Items.SNOWBALL), new ItemStack(Items.COD), EntityType.POLAR_BEAR, ExCompressumConfig.COMMON.polarBearBaitChance::get),
-    LLAMA(new ItemStack(Items.WHEAT), new ItemStack(Items.SUGAR), EntityType.LLAMA, ExCompressumConfig.COMMON.llamaBaitChance::get),
-    PARROT(new ItemStack(Items.RED_DYE), new ItemStack(Items.GREEN_DYE), EntityType.PARROT, ExCompressumConfig.COMMON.parrotBaitChance::get),
-    CAT(new ItemStack(Items.RED_DYE), new ItemStack(Items.GREEN_DYE), EntityType.CAT, ExCompressumConfig.COMMON.catBaitChance::get), // TODO fix items
-    FOX(new ItemStack(Items.RED_DYE), new ItemStack(Items.GREEN_DYE), EntityType.FOX, ExCompressumConfig.COMMON.foxBaitChance::get),
-    TURTLE(new ItemStack(Items.RED_DYE), new ItemStack(Items.GREEN_DYE), EntityType.TURTLE, ExCompressumConfig.COMMON.turtleBaitChance::get),
-    MOOSHROOM(new ItemStack(Items.RED_DYE), new ItemStack(Items.GREEN_DYE), EntityType.MOOSHROOM, ExCompressumConfig.COMMON.mooshroomBaitChance::get);
+    WOLF(new ItemStack(Items.BEEF), new ItemStack(Items.BONE), EntityType.WOLF, ExCompressumConfig.COMMON.wolfBaitChance::get, 14144467, 13545366),
+    OCELOT(new ItemStack(Items.GUNPOWDER), new ItemStack(Items.COD), EntityType.OCELOT, ExCompressumConfig.COMMON.ocelotBaitChance::get, 15720061, 5653556),
+    COW(new ItemStack(Items.WHEAT), new ItemStack(Items.WHEAT), EntityType.COW, ExCompressumConfig.COMMON.cowBaitChance::get, 4470310, 10592673),
+    PIG(new ItemStack(Items.CARROT), new ItemStack(Items.CARROT), EntityType.PIG, ExCompressumConfig.COMMON.pigBaitChance::get, 15771042, 14377823),
+    CHICKEN(new ItemStack(Items.WHEAT_SEEDS), new ItemStack(Items.WHEAT_SEEDS), EntityType.CHICKEN, ExCompressumConfig.COMMON.chickenBaitChance::get,  10592673, 16711680),
+    SHEEP(new ItemStack(Items.WHEAT_SEEDS), new ItemStack(Items.WHEAT), EntityType.SHEEP, ExCompressumConfig.COMMON.sheepBaitChance::get,  15198183, 16758197),
+    SQUID(new ItemStack(Items.COD), new ItemStack(Items.COD), EntityType.SQUID, ExCompressumConfig.COMMON.squidBaitChance::get,  2243405, 7375001),
+    RABBIT(new ItemStack(Items.CARROT), new ItemStack(Items.MELON_SEEDS), EntityType.RABBIT, ExCompressumConfig.COMMON.rabbitBaitChance::get,  10051392, 7555121),
+    HORSE(new ItemStack(Items.GOLDEN_APPLE), new ItemStack(Items.GOLDEN_APPLE), EntityType.HORSE, ExCompressumConfig.COMMON.horseBaitChance::get,  6842447, 15066584),
+    DONKEY(new ItemStack(Items.GOLDEN_CARROT), new ItemStack(Items.GOLDEN_CARROT), EntityType.DONKEY, ExCompressumConfig.COMMON.donkeyBaitChance::get,  5457209, 8811878),
+    POLAR_BEAR(new ItemStack(Items.SNOWBALL), new ItemStack(Items.COD), EntityType.POLAR_BEAR, ExCompressumConfig.COMMON.polarBearBaitChance::get,  15921906, 9803152),
+    LLAMA(new ItemStack(Items.WHEAT), new ItemStack(Items.SUGAR), EntityType.LLAMA, ExCompressumConfig.COMMON.llamaBaitChance::get,  15377456, 4547222),
+    PARROT(new ItemStack(Items.RED_DYE), new ItemStack(Items.GREEN_DYE), EntityType.PARROT, ExCompressumConfig.COMMON.parrotBaitChance::get,  894731, 16711680),
+    CAT(new ItemStack(Items.LEAD), new ItemStack(Items.COD), EntityType.CAT, ExCompressumConfig.COMMON.catBaitChance::get,  15714446, 9794134),
+    FOX(new ItemStack(Items.RABBIT), new ItemStack(Items.SWEET_BERRIES), EntityType.FOX, ExCompressumConfig.COMMON.foxBaitChance::get,  14005919, 13396256),
+    TURTLE(new ItemStack(Items.SEAGRASS), new ItemStack(Items.SEA_PICKLE), EntityType.TURTLE, ExCompressumConfig.COMMON.turtleBaitChance::get,  15198183, 44975),
+    MOOSHROOM(new ItemStack(Items.RED_MUSHROOM), new ItemStack(Items.WHEAT), EntityType.MOOSHROOM, ExCompressumConfig.COMMON.mooshroomBaitChance::get,  10489616, 12040119);
 
     private final ItemStack displayItemFirst;
     private final ItemStack displayItemSecond;
     private final EntityType<?> entityType;
     private final Supplier<Double> chanceSupplier;
+    private final int primaryColor;
+    private final int secondaryColor;
     private List<BaitEnvironmentCondition> environmentConditions;
 
-    BaitType(ItemStack displayItemFirst, ItemStack displayItemSecond, EntityType<?> entityType, Supplier<Double> chanceSupplier) {
+    BaitType(ItemStack displayItemFirst, ItemStack displayItemSecond, EntityType<?> entityType, Supplier<Double> chanceSupplier, int primaryColor, int secondaryColor) {
         this.displayItemFirst = displayItemFirst;
         this.displayItemSecond = displayItemSecond;
         this.entityType = entityType;
         this.chanceSupplier = chanceSupplier;
+        this.primaryColor = primaryColor;
+        this.secondaryColor = secondaryColor;
     }
 
     @Override
@@ -117,5 +121,10 @@ public enum BaitType implements IStringSerializable {
         }
 
         return environmentConditions;
+    }
+
+    public int getItemColor(int tintIndex) {
+
+        return tintIndex == 0 ? primaryColor : secondaryColor;
     }
 }
