b2c38382da0b91338e9f5493167ddfb8ae09a206
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-05 22:24:19 +0100

    fix: Fix compats not being loaded on NeoForge

--- common/src/main/java/net/blay09/mods/excompressum/ExCompressum.java
+++ common/src/main/java/net/blay09/mods/excompressum/ExCompressum.java
@@ -43,8 +43,8 @@ public class ExCompressum {
         ModLoot.initialize(Balm.getLootTables());
         ModRecipeTypes.initialize(Balm.getRecipes());
 
-        Balm.initializeIfLoaded(Compat.EXNIHILO_SEQUENTIA, "net.blay09.mods.excompressum.forge.compat.exnihilosequentia.ExNihiloSequentiaAddon");
-        Balm.initializeIfLoaded(Compat.EX_DEORUM, "net.blay09.mods.excompressum.forge.compat.exdeorum.ExDeorumAddon");
+        Balm.initializeIfLoaded(Compat.EXNIHILO_SEQUENTIA, "net.blay09.mods.excompressum.neoforge.compat.exnihilosequentia.ExNihiloSequentiaAddon");
+        Balm.initializeIfLoaded(Compat.EX_DEORUM, "net.blay09.mods.excompressum.neoforge.compat.exdeorum.ExDeorumAddon");
         Balm.initializeIfLoaded(Compat.FABRICAE_EX_NIHILO, "net.blay09.mods.excompressum.fabric.compat.fabricaeexnihilo.FabricaeExNihiloAddon");
 
         AutoSieveSkinRegistry.load();
