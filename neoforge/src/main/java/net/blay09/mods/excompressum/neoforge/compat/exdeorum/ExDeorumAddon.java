ce4b0f2253273e8a5718f03e3b439f2e18b530ed
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-07 22:23:48 +0100

    fix: Adjust crook recipe chance in Ex Deorum support to match the mod more closely #383

--- neoforge/src/main/java/net/blay09/mods/excompressum/neoforge/compat/exdeorum/ExDeorumAddon.java
+++ neoforge/src/main/java/net/blay09/mods/excompressum/neoforge/compat/exdeorum/ExDeorumAddon.java
@@ -23,6 +23,7 @@ import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.Item;
@@ -195,13 +196,15 @@ public class ExDeorumAddon implements ExNihiloProvider {
 
     @Override
     public List<ItemStack> rollCrookRewards(ServerLevel level, BlockPos pos, BlockState state, @Nullable Entity entity, ItemStack tool, RandomSource rand) {
-        final float luck = getLuckFromTool(level, tool);
+        final float fortune = getLuckFromTool(level, tool);
         final var recipes = RecipeUtil.getCrookRecipes(state);
         List<ItemStack> list = new ArrayList<>();
         for (final var recipe : recipes) {
-            float fortuneChanceBonus = 0.1f;
-            if (rand.nextFloat() <= recipe.chance() + fortuneChanceBonus * luck) {
-                list.add(recipe.getResultItem(level.registryAccess()));
+            int rolls = Math.max(1, Mth.ceil(fortune / 3f));
+            for (int i = 0; i < rolls; i++) {
+                if (rand.nextFloat() < recipe.chance()) {
+                    list.add(recipe.getResultItem(level.registryAccess()));
+                }
             }
         }
         return list;
